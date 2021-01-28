d63acf173c0f24ddef1b8c5df97939a6fe65c319
Sergey <sergey_chizhik@ukr.net>
2021-01-28 14:36:02 +0200

    Liked posts

--- client/src/containers/Profile/index.jsx
+++ client/src/containers/Profile/index.jsx
@@ -1,39 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getUserImgLink } from 'src/helpers/imageHelper';
 import {
   Grid,
   Image,
-  Input
+  Input,
+  Button
 } from 'semantic-ui-react';
 
-const Profile = ({ user }) => (
-  <Grid container textAlign="center" style={{ paddingTop: 30 }}>
-    <Grid.Column>
-      <Image centered src={getUserImgLink(user.image)} size="medium" circular />
-      <br />
-      <Input
-        icon="user"
-        iconPosition="left"
-        placeholder="Username"
-        type="text"
-        disabled
-        value={user.username}
-      />
-      <br />
-      <br />
-      <Input
-        icon="at"
-        iconPosition="left"
-        placeholder="Email"
-        type="email"
-        disabled
-        value={user.email}
-      />
-    </Grid.Column>
-  </Grid>
-);
+/* eslint-disable */
+const Profile = ({ user }) => {
+  const [isNotEditMode, setEditMode] = useState(true);
+
+  const toggleProfileMode = () => {
+    setEditMode(!isNotEditMode);
+  };
+
+  return (
+    <Grid container textAlign="center" style={{ paddingTop: 30 }}>  
+      <Grid.Column>
+        <Image centered src={getUserImgLink(user.image)} size="medium" circular />
+        <br />
+        <Input
+          icon="user"
+          iconPosition="left"
+          placeholder="Username"
+          type="text"
+          disabled={isNotEditMode}
+          value={user.username}
+        />
+        <br />
+        <br />
+        <Input
+          icon="at"
+          iconPosition="left"
+          placeholder="Email"
+          type="email"
+          disabled={isNotEditMode}
+          value={user.email}
+        />
+        <br />
+        <br />
+        <Button type="button" color="teal" size="large" primary onClick={toggleProfileMode}>
+          Edit profile
+        </Button>
+      </Grid.Column>
+    </Grid>
+  )
+};
 
 Profile.propTypes = {
   user: PropTypes.objectOf(PropTypes.any)
